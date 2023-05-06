e9c49f71e10659c0846d6570d1213cec0f8fea4d
becardine <bealvescardine@gmail.com>
2023-05-06 12:42:13 -0300

    feat: make course controller and factory

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\{CourseController};
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/courses', [CourseController::class, 'index']);
 
 Route::get('/', function () {
     return response()->json([
