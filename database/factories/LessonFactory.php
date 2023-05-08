e4c220849ba1805bd7d6cb180f7669c14a8d6859
becardine <bealvescardine@gmail.com>
2023-05-08 00:46:03 -0300

    feat: factories for modules and lessons

---
+++ database/factories/LessonFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Lesson;
+use App\Models\Module;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends Factory<Lesson>
+ */
+class LessonFactory extends Factory
+{
+    protected $model = Lesson::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $name = $this->faker->name();
+        return [
+            'name' => $name,
+            'url' => Str::slug($name),
+            'video' => Str::random(),
+            'description' => $this->faker->sentence(20),
+            'module_id' => Module::factory()
+        ];
+    }
+}
---
+++ database/factories/ModuleFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Course;
+use App\Models\Module;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Module>
+ */
+class ModuleFactory extends Factory
+{
+    protected $model = Module::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'course_id' => Course::factory()
+        ];
+    }
+}
