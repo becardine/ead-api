67aa03136f905bd5a0254796ed315a46005715e5
becardine <bealvescardine@gmail.com>
2023-05-08 23:41:20 -0300

    feat: create reset password

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Models\Traits\UuidTrait;
+use App\Notifications\ResetPasswordNotification;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -46,6 +47,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new ResetPasswordNotification($token));
+    }
 
     public function supports()
     {
--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,34 @@
 <?php
 
-use App\Http\Controllers\{Auth\AuthController,
+use App\Http\Controllers\{
     CourseController,
     LessonController,
     ModuleController,
     ReplySupportController,
     SupportController};
-use Illuminate\Http\Request;
+
+use App\Http\Controllers\Auth\{
+    AuthController,
+    ResetPasswordController};
+
 use Illuminate\Support\Facades\Route;
 
+/*
+ * Auth
+ */
 Route::post('/auth', [AuthController::class, 'auth']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::get('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
 
+/*
+ * Reset password
+ */
+Route::post('/forgot-password', [ResetPasswordController::class, 'sendResetLink'])->middleware('guest');
+
+
+/*
+ * Api
+ */
 Route::middleware(['auth:sanctum'])->group(function () {
 
     Route::get('/courses', [CourseController::class, 'index']);
